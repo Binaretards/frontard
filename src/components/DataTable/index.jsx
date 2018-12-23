3517cb6ea68f2a61be8b8f37352202f438c4c2ff
Redbore <mariyzdede@gmail.com>
2018-12-23 17:00:46 +0300

    added all api methods

--- src/components/DataTable/index.jsx
+++ src/components/DataTable/index.jsx
@@ -1,165 +1,169 @@
-import React, { Component } from 'react'
-import { Table, Button, Alert } from 'reactstrap'
+import React, {Component} from 'react'
+import {Alert, Button, Table} from 'reactstrap'
 import _ from 'lodash'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {ApiService} from "../../services/api.service";
 
 const data = {
-  "tableName": [
-      {
-        "uuid": "d708ea20-96ed-412b-82f1-43a177fbeeca",
-        "fields": {
-          "id": 1,
-          "path": "/img/img_1.tga",
-          "scale_x": 0.25,
-          "scale_y": 0.25
-        }
-      },
-      {
-        "uuid": "d708ea20-96ed-412b-82f1-43a177fbeecb",
-        "fields": {
-          "id": 2,
-          "path": "/img/img_1111.tga",
-          "scale_x": 0.225,
-          "scale_y": 0.255
-        }
-      },
-      {
-        "uuid": "d708ea20-96ed-412b-82f1-43a177fbeecс",
-        "fields": {
-          "id": 555,
-          "path": "/шьтфу",
-          "scale_x": 0.322,
-          "scale_y": 2.255
-        }
-      },
+    "tableName": [
+        {
+            "uuid": "d708ea20-96ed-412b-82f1-43a177fbeeca",
+            "fields": {
+                "id": 1,
+                "path": "/img/img_1.tga",
+                "scale_x": 0.25,
+                "scale_y": 0.25
+            }
+        },
+        {
+            "uuid": "d708ea20-96ed-412b-82f1-43a177fbeecb",
+            "fields": {
+                "id": 2,
+                "path": "/img/img_1111.tga",
+                "scale_x": 0.225,
+                "scale_y": 0.255
+            }
+        },
+        {
+            "uuid": "d708ea20-96ed-412b-82f1-43a177fbeecс",
+            "fields": {
+                "id": 555,
+                "path": "/шьтфу",
+                "scale_x": 0.322,
+                "scale_y": 2.255
+            }
+        },
     ],
-  "table2": [
-      {
-        "uuid": "d708ea20-96ed-412b-82f1-43a177fbeeca",
-        "fields": {
-          "privet": 1,
-          "put": "/img/img_2.tga",
-          "scally": 0.25,
-          "mauldar": 0.25
-        }
-      },
+    "table2": [
+        {
+            "uuid": "d708ea20-96ed-412b-82f1-43a177fbeeca",
+            "fields": {
+                "privet": 1,
+                "put": "/img/img_2.tga",
+                "scally": 0.25,
+                "mauldar": 0.25
+            }
+        },
     ]
 }
 
 class TableList extends Component {
+    apiService = new ApiService();
 
-  constructor(props) {
-    super(props)
-    this.state = {
-      data: [],
-      loaded: false,
-      activeTab: Object.keys(data)[0]
+    constructor(props) {
+        super(props)
+        this.state = {
+            data: [],
+            loaded: false,
+            activeTab: Object.keys(data)[0]
+        }
     }
-  }
 
-  componentDidMount() {
-    this.setState({ data, loaded: true })
-  }
-
-  showTabs = (table) => {
-    const tables = Object.keys(data)
-    return tables.map((item, i) => {
-      console.log(item)
-      let activeProperty = item === this.state.activeTab ? { active: true } : {}
-      return (
-        <Button color="info" onClick={() => this.setState({ activeTab: item })} {...activeProperty} key={i}>
-          {item}
-        </Button>
-      )
-    })
-  }
-
-  showActiveTab = () => {
-    let activeTab = this.state.activeTab
-    let tableData = this.state.data[activeTab]
-
-    return (
-      this.renderTable(activeTab, tableData)
-    )
-  }
-
-  renderTable = (tableName, tableData) => {
-    console.log(tableData)
-    if(!tableData || !tableData.length){
-      return (
-        <Alert>
-          Нет данных
-        </Alert>
-      )
+    componentDidMount() {
+        this.setState({data, loaded: true})
     }
-    let header = Object.keys(tableData[0].fields)
 
-    return (
-      <Table bordered>
-        <thead>
-          <tr>
-            {header.map((item, index) => {
-              return (
-                <th key={index}>{item}</th>
-              )
-            })}
-            <th></th>
-          </tr>
-        </thead>
-        <tbody>
-          {tableData.map(item => {
+    showTabs = (table) => {
+        const tables = Object.keys(data)
+        return tables.map((item, i) => {
+            console.log(item)
+            let activeProperty = item === this.state.activeTab ? {active: true} : {}
             return (
-              <tr key={item.uuid}>
-                {Object.keys(item.fields).map((key, i) => {
-                  let field = item.fields[key]
-                  return (
-                    <td key={i}>
-                      <input className="form-control" type="text" value={field}
-                      onChange={(event) => {this.onInputChange(tableName, item.uuid, key, event.target.value)}}>
-                      </input>
-                    </td>
-                  )
-                })}
-                 <td><Button onClick={() => this.onRowDelete(tableName, item.uuid)} outline>X</Button></td>
-              </tr>
+                <Button color="info" onClick={() => this.setState({activeTab: item})} {...activeProperty} key={i}>
+                    {item}
+                </Button>
             )
-          })}
-        </tbody>
-      </Table>
-    )
-  }
-
-  onRowDelete = (tableName, rowId) => {
-    let stateData = _.cloneDeep(this.state.data)
-    let rows = stateData[tableName].filter(item => item.uuid !== rowId)
-    stateData[tableName] = rows
-    this.setState({data: stateData})
-  }
-
-  onInputChange = (tableName, rowId, fieldName, value) => {
-    console.log('oninputchange', tableName, rowId, fieldName, value)
-    let stateData = _.cloneDeep(this.state.data)
-    let rows = stateData[tableName]
-    let index = _.findIndex(rows, {uuid: rowId})
-    if(~index){
-      rows[index].fields[fieldName] = value
+        })
     }
-    stateData = rows
-    this.setState({data: stateData})
-  }
 
-  render() {
-    if (!this.state.loaded) {
-      return null
+    showActiveTab = () => {
+        let activeTab = this.state.activeTab
+        let tableData = this.state.data[activeTab]
+
+        return (
+            this.renderTable(activeTab, tableData)
+        )
     }
-    console.log(this)
-    return (
-      <div>
-        {this.showTabs()}
-        {this.showActiveTab()}
-      </div>
-    )
-  }
+
+    renderTable = (tableName, tableData) => {
+        console.log(tableData)
+        if (!tableData || !tableData.length) {
+            return (
+                <Alert>
+                    Нет данных
+                </Alert>
+            )
+        }
+        let header = Object.keys(tableData[0].fields)
+
+        return (
+            <Table bordered>
+                <thead>
+                <tr>
+                    {header.map((item, index) => {
+                        return (
+                            <th key={index}>{item}</th>
+                        )
+                    })}
+                    <th></th>
+                </tr>
+                </thead>
+                <tbody>
+                {tableData.map(item => {
+                    return (
+                        <tr key={item.uuid}>
+                            {Object.keys(item.fields).map((key, i) => {
+                                let field = item.fields[key]
+                                return (
+                                    <td key={i}>
+                                        <input className="form-control" type="text" value={field}
+                                               onChange={(event) => {
+                                                   this.onInputChange(tableName, item.uuid, key, event.target.value)
+                                               }}>
+                                        </input>
+                                    </td>
+                                )
+                            })}
+                            <td><Button onClick={() => this.onRowDelete(tableName, item.uuid)} outline>X</Button></td>
+                        </tr>
+                    )
+                })}
+                </tbody>
+            </Table>
+        )
+    }
+
+    onRowDelete = (tableName, rowId) => {
+        let stateData = _.cloneDeep(this.state.data)
+        let rows = stateData[tableName].filter(item => item.uuid !== rowId)
+        stateData[tableName] = rows
+        this.setState({data: stateData})
+    }
+
+    onInputChange = (tableName, rowId, fieldName, value) => {
+        console.log('oninputchange', tableName, rowId, fieldName, value)
+        let stateData = _.cloneDeep(this.state.data)
+        let rows = stateData[tableName]
+        let index = _.findIndex(rows, {uuid: rowId})
+        if (~index) {
+            rows[index].fields[fieldName] = value
+        }
+        stateData = rows
+        this.setState({data: stateData})
+    }
+
+    render() {
+        if (!this.state.loaded) {
+            return null
+        }
+        console.log(this)
+        return (
+            <div>
+                {this.showTabs()}
+                {this.showActiveTab()}
+            </div>
+        )
+    }
 
 }
 
--- src/services/api.service.js
+++ src/services/api.service.js
@@ -1,11 +1,59 @@
 import axios from 'axios';
 
 export class ApiService {
-    host: String = 'http://localhost:8080';
+    host: String = 'http://localhost:8080/api';
+
+    ping() {
+        axios
+            .post(`${this.host}/ping`)
+            .then(response => console.log(response.data));
+    }
 
     getTables() {
         axios
-            .get(`${this.host}/api/tables/get`)
+            .get(`${this.host}/tables/get`)
+            .then(response => console.log(response.data));
+    }
+
+    saveTables() {
+        axios
+            .get(`${this.host}/tables/save`)
+            .then(response => console.log(response.data));
+    }
+
+    discardTables() {
+        axios
+            .post(`${this.host}/tables/discard`)
+            .then(response => console.log(response.data));
+    }
+
+    savePaths() {
+        axios
+            .post(`${this.host}/paths/save`)
+            .then(response => console.log(response.data));
+    }
+
+    getPaths() {
+        axios
+            .get(`${this.host}/paths/get`)
+            .then(response => console.log(response.data));
+    }
+
+    addRows() {
+        axios
+            .post(`${this.host}/rows/add`)
+            .then(response => console.log(response.data));
+    }
+
+    removeRows() {
+        axios
+            .post(`${this.host}/rows/remove`)
+            .then(response => console.log(response.data));
+    }
+
+    editFields() {
+        axios
+            .post(`${this.host}/fields/edit`)
             .then(response => console.log(response.data));
     }
 }
